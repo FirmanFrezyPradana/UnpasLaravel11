a02a35b2a8d6ff2a75848bf28cf13ab66a0c2a84
“FirmanFrezyPradana” <dana.firman16@gmail.com>
2024-07-06 10:30:05 +0700

    model factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,10 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            // $a=$a ? $a : $b  ternary operator
+            // $a = $a?:$b; elvis operator
+            // $a ??=$b null collecting operator
+
             'remember_token' => Str::random(10),
         ];
     }
